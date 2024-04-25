19bb905ac6a4314f887485b826a206dd57a8c239
S.Samarsky <js.samarsky@gmail.com>
2024-04-25 14:49:37 +0300

    feat: add btns start reset

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import Field from "./classes/field";
 import Canvas from "./classes/canvas";
-import './style.css';
+import './style.scss';
 import Snake from "./classes/snake";
 import Food from "./classes/food";
 import Game from "./classes/game";
 
 const root = document.querySelector('#app');
+
 const field = new Field(20, 20, 20, false);
 const canvas = new Canvas();
 canvas.create(field.width, field.height, root);
@@ -17,14 +18,20 @@ const food = new Food('orange');
 
 const game = new Game();
 
+const btnStart = document.querySelector('#btn-start');
+const btnReset = document.querySelector('#btn-reset');
+
 document.addEventListener('keydown', (e) => {
     snake.setDirection(e);
+    btnStart!.textContent = 'Pause';
 
     if (e.key === ' ' || e.key === 'Enter') {
         if (game.isPause) {
             game.play(snake, canvas, field, food);
+            btnStart!.textContent = 'Pause';
         } else if (game.isPlay) {
             game.pause(snake, food);
+            btnStart!.textContent = 'Play';
         }
 
         game.start(snake, canvas, field, food);
@@ -33,6 +40,7 @@ document.addEventListener('keydown', (e) => {
     const keysReset = ['R', 'r', 'К', 'к'];
     if (keysReset.includes(e.key)) {
         game.reset(snake, canvas, field, food);
+        btnStart!.textContent = 'Play';
     }
     
     const keysToggleSpeed = ['E', 'e', 'У', 'у'];
@@ -40,3 +48,23 @@ document.addEventListener('keydown', (e) => {
         snake.toggleSpeed(2, canvas, field, food, game);
     }
 });
+
+btnStart?.addEventListener('click', () => {
+    if (!game.isStart) {
+        game.start(snake, canvas, field, food);
+        btnStart.textContent = 'Pause';
+    } else {
+        if (game.isPause) {
+            game.play(snake, canvas, field, food);
+            btnStart.textContent = 'Pause';
+        } else if (game.isPlay) {
+            game.pause(snake, food);
+            btnStart.textContent = 'Play';
+        }
+    }
+});
+
+btnReset?.addEventListener('click', () => {
+    game.reset(snake, canvas, field, food);
+    btnStart!.textContent = 'Play';
+})
